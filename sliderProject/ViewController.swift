e0f4a037adc54cabb85e9113278160d8fda0a1da
MaximLaskin <maxlaskin@mail.ru>
2022-08-05 19:08:17 +0300

    Initial Commit

---
+++ sliderProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sliderProject
+//
+//  Created by Swift on 05.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
